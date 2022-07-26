5889e6cd70d2e854afe3b91634a0774db989ea1d
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2022-07-26 19:50:51 -0400

    Import CSV, name columns, and clean up data

--- bookmarks/main.py
+++ bookmarks/main.py
@@ -1,3 +1,15 @@
 import pandas as pd
 
-cars = pd.read_csv("bookmarks.csv")
+col_names = ['Category',
+             'Subcategory',
+             'URL',
+             'Company Name',
+             'Tag Line',
+             'Description']
+
+bookmarks = pd.read_csv('bookmarks.csv', names=col_names)
+
+bookmarks.pop('Category')
+bookmarks.pop('Subcategory')
+
+print(bookmarks.head())
