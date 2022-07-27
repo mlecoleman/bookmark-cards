3c781ce43122cb6cc4c49308601f9289f59422fd
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2022-07-26 21:30:39 -0400

    Add items that search through bookmarks and return information based on searches

--- bookmarks/main.py
+++ bookmarks/main.py
@@ -1,15 +1,41 @@
 import pandas as pd
 
-col_names = ['Category',
-             'Subcategory',
-             'URL',
-             'Company Name',
-             'Tag Line',
-             'Description']
+# Name columns since columns in CSV aren't named
+col_names = ['category',
+             'subcategory',
+             'url',
+             'name',
+             'tagline',
+             'description']
 
+# Read in CSV file of bookmarks
 bookmarks = pd.read_csv('bookmarks.csv', names=col_names)
 
-bookmarks.pop('Category')
-bookmarks.pop('Subcategory')
+# Remove these columns because they are incomplete
+bookmarks.pop('category')
+bookmarks.pop('subcategory')
 
-print(bookmarks.head())
+
+# Look to see how many rows contain a certain key word in their description column
+bike = bookmarks['description'].str.contains('bike').sum()
+plant = bookmarks['description'].str.contains('plant').sum()
+louisville = bookmarks['description'].str.contains('louisville').sum()
+
+# Search for rows based on certain key-words, would be useful if I'm looking for a specific resource
+bicycle = bookmarks[bookmarks['description'].str.lower().str.contains('bicycle')]
+louisville = bookmarks[bookmarks['description'].str.lower().str.contains('louisville')]
+
+# Total number of bookmarks for reference
+length = len(bookmarks)
+
+# filtered_row = bookmarks[ bookmarks["description"]("bike")]
+print(bike)
+print(plant)
+print(louisville)
+print(bicycle)
+print(length)
+
+
+
+
+
